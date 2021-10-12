28fb25be70fc6ecbd73625516d67db7db1e7258c
GijsVj <gijs.verjans@student.ehb.be>
2021-10-12 17:32:08 +0200

    Challlenge 2 finished

--- adventure-game/src/main/kotlin/adventure-game.kt
+++ adventure-game/src/main/kotlin/adventure-game.kt
@@ -5,35 +5,49 @@ fun main () {
 }
 
 fun challengeOne() {
-    val options = arrayOf("close combat", "keep distance and shoot", "try to talk")
-    val userChoice = getUserChoice1(options)
-    println("User picked $userChoice")
-    determineWinner1(userChoice)
+    val answers = setOf<String>("close combat", "keep distance and shoot", "try to talk")
+    println("What are you gonna do?: close combat - keep distance and shoot - try to talk")
+    val userAnswer = readLine()
+
+    if (answers.contains(userAnswer)) {
+        println("Good job you're Iron man armor is too strong for Clint to beat in hand-to-hand combat")
+        println("You hit him in the head and he is back")
+        println("HAWKEYE JOINED YOUR TEAM!!")
+        println("You will be Hawkeye for the next battle")
+        challengeTwo()
+
+    } else {
+        gameover()
+    }
 
 }
 
-fun getUserChoice1 (options: Array<String>):String? {
-    var userChoice: String?
-    do {
-        println("You run into a mind controlled Black Widow aka Natasha Romanoff")
-        println("What are you gonna do?: close combat - keep distance and shoot - try to talk")
-        userChoice = readLine()
-    } while (!options.contains(userChoice))
-
-    return  userChoice
-
-}
-
-fun determineWinner1(userChoice: String?){
-    if ("keep distance and shoot" == userChoice){
-        println("Good job a shot in the head brought her back")
-        println("BLACK WIDOW JOINED YOUR TEAM!!")
+fun challengeTwo() {
+    println("You run into a mind controlled Black Widow aka Natasha Romanoff")
+    println("Try to hit her in the hit her in the right zone with an arrow")
+    println("choose: low - high")
+    val diceResult = (1..6).random()
+    val userAnswer = readLine()
+    if (userAnswer == "low" && (1..3).contains(diceResult)) {
+        println("Good shot Hawkeye! You hit her in the right position and freed her from her mind controle")
+        challengeThree()
+    } else if (userAnswer == "high" && (1..3).contains(diceResult)) {
+        println("Good shot Hawkeye! You hit her in the right position and freed her from her mind controle")
+        challengeThree()
     } else {
         gameover()
     }
 }
 
+fun challengeThree() {
+    println("You will play as Black Widow for the next battle")
+    println("You run into Loki who is with The incredible Hulk aka Doctor Bruce Banner")
+}
+
 fun gameover() {
-    println("That was not good choice hero try again and better luck next time")
+    println("That was not good hero try again and better luck next time!")
     return
 }
+
+
+
